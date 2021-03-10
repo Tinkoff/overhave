f69644b8f640cd43f966797634e79087bb03f411
Artemy Rogov <57125077+artamaney@users.noreply.github.com>
2021-03-10 21:31:23 +0500

    try to fix x9

--- tests/integration/storage/test_emulation.py
+++ tests/integration/storage/test_emulation.py
@@ -17,50 +17,49 @@ class TestEmulation:
     def test_emulation_storage(self, test_emulation_settings) -> e.EmulationStorage:
         return e.EmulationStorage(test_emulation_settings)
 
-    @pytest.fixture
     def test_add_emulation_to_db(self):
         # TRASH????????
         emulation: db.tables.Emulation = db.tables.Emulation()
         emulation.id = 1
-        emulation.name = 'test'
-        emulation.command = 'test'
-        emulation.created_by = 'admin'
-        emulation.test_user_id = 0
+        emulation.name = "test"
+        emulation.command = "test"
+        emulation.created_by = "admin"
+        emulation.test_user_id = 1
         test_user = db.tables.TestUser()
-        test_user.id = 0
-        test_user.created_by = 'admin'
-        test_user.feature_type_id = 0
-        test_user.name = 'test'
+        test_user.id = 1
+        test_user.created_by = "admin"
+        test_user.feature_type_id = 1
+        test_user.name = "test"
         feature_type = db.tables.FeatureType()
-        feature_type.name = 'test'
-        feature_type.id = -10
+        feature_type.name = "test"
+        feature_type.id = 1
         with db.create_session() as session:
             session.add(feature_type)
             session.add(test_user)
             session.add(emulation)
             session.flush()
 
-    def test_raise_exception_for_not_existing_id(self, test_emulation_storage, test_add_emulation_to_db):
+    def test_raise_exception_for_not_existing_id(self, test_emulation_storage):
         test_emulation_storage.create_emulation_run(1, "admin")
         with pytest.raises(e.NotFoundEmulationError):
             test_emulation_storage.get_requested_emulation_run(-1)
 
-    def test_create_emulation_run(self, test_emulation_storage, test_add_emulation_to_db):
+    def test_create_emulation_run(self, test_emulation_storage):
         emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(1, "admin")
         assert emulation_run.status == EmulationStatus.CREATED
         assert emulation_run.emulation_id == 1
         assert emulation_run.initiated_by == "admin"
         # Checking port?
 
-    def test_get_requested_emulation_run(self, test_emulation_storage, test_add_emulation_to_db):
+    def test_get_requested_emulation_run(self, test_emulation_storage):
         test_emulation_storage.create_emulation_run(1, "admin")
         requested_emulation_run: db.EmulationRun = test_emulation_storage.get_requested_emulation_run(1)
         assert requested_emulation_run.status == EmulationStatus.REQUESTED
         assert requested_emulation_run.emulation_id == 1
         # Checking port?
 
-    def test_set_error_run_status(self, test_emulation_storage, test_add_emulation_to_db):
+    def test_set_error_run_status(self, test_emulation_storage):
         emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(1, "admin")
         assert emulation_run.status == EmulationStatus.CREATED
-        test_emulation_storage.set_error_emulation_run(1, "test set_error_emulation_run")
+        test_emulation_storage.set_error_emulation_run(1, "admin")
         assert emulation_run.status == EmulationStatus.ERROR
