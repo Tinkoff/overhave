f69f916bb97e1aa7889cf8d1e892080746a7e757
Artemy Rogov <57125077+artamaney@users.noreply.github.com>
2021-03-11 11:05:10 +0500

    add conftest.py

---
+++ tests/integration/storage/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from overhave.entities.settings import OverhaveEmulationSettings
+import overhave.storage.emulation as e
+from faker import Faker
+from overhave import db
+
+ADMIN = "admin"
+
+
+@pytest.fixture()
+def test_emulation_settings() -> OverhaveEmulationSettings:
+    return OverhaveEmulationSettings()
+
+
+@pytest.fixture()
+def test_emulation_storage(test_emulation_settings) -> e.EmulationStorage:
+    return e.EmulationStorage(test_emulation_settings)
+
+
+@pytest.fixture()
+def test_add_emulation_to_db():
+    faker = Faker()
+    feature_type = db.tables.FeatureType(name=faker.word())
+    test_user = db.tables.TestUser(created_by=ADMIN, feature_type_id=feature_type.id, name=faker.word())
+    emulation: db.tables.Emulation = db.tables.Emulation(
+        name=faker.word(), command=faker.word(), created_by=ADMIN, test_user_id=test_user.id
+    )
+    with db.create_session() as session:
+        session.add(feature_type)
+        session.add(test_user)
+        session.add(emulation)
+    return emulation.id
--- tests/integration/storage/test_emulation.py
+++ tests/integration/storage/test_emulation.py
@@ -2,65 +2,40 @@ import pytest
 
 import overhave.storage.emulation as e
 from overhave import db
+from faker import Faker
 from overhave.db.statuses import EmulationStatus
-from overhave.entities.settings import OverhaveEmulationSettings
+from tests.integration.storage.conftest import ADMIN
 
 
-class TestEmulation:
-    """Integration tests for emulation"""
-
-    @pytest.fixture()
-    def test_emulation_settings(self) -> OverhaveEmulationSettings:
-        return OverhaveEmulationSettings()
-
-    @pytest.fixture()
-    def test_emulation_storage(self, test_emulation_settings) -> e.EmulationStorage:
-        return e.EmulationStorage(test_emulation_settings)
-
-    @pytest.fixture()
-    def test_add_emulation_to_db(self):
-        # TRASH????????
-        emulation: db.tables.Emulation = db.tables.Emulation()
-        emulation.id = 1
-        emulation.name = "test"
-        emulation.command = "test"
-        emulation.created_by = "admin"
-        emulation.test_user_id = 1
-        test_user = db.tables.TestUser()
-        test_user.id = 1
-        test_user.created_by = "admin"
-        test_user.feature_type_id = 1
-        test_user.name = "test"
-        feature_type = db.tables.FeatureType()
-        feature_type.name = "test"
-        feature_type.id = 1
-        with db.create_session() as session:
-            session.add(feature_type)
-            session.add(test_user)
-            session.add(emulation)
-            session.flush()
+class TestEmulationStorage:
+    """ Integration tests for :class:`EmulationStorage`. """
 
     def test_raise_exception_for_not_existing_id(self, test_emulation_storage, test_add_emulation_to_db):
-        test_emulation_storage.create_emulation_run(1, "admin")
+        test_emulation_storage.create_emulation_run(emulation_id=test_add_emulation_to_db, initiated_by=ADMIN)
         with pytest.raises(e.NotFoundEmulationError):
             test_emulation_storage.get_requested_emulation_run(-1)
 
     def test_create_emulation_run(self, test_emulation_storage, test_add_emulation_to_db):
-        emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(1, "admin")
+        emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(
+            emulation_id=test_add_emulation_to_db, initiated_by=ADMIN
+        )
         assert emulation_run.status == EmulationStatus.CREATED
-        assert emulation_run.emulation_id == 1
-        assert emulation_run.initiated_by == "admin"
-        # Checking port?
+        assert emulation_run.emulation_id == test_add_emulation_to_db
+        assert emulation_run.initiated_by == ADMIN
+        assert emulation_run.port is None
 
     def test_get_requested_emulation_run(self, test_emulation_storage, test_add_emulation_to_db):
-        test_emulation_storage.create_emulation_run(1, "admin")
+        test_emulation_storage.create_emulation_run(emulation_id=test_add_emulation_to_db, initiated_by=ADMIN)
         requested_emulation_run: db.EmulationRun = test_emulation_storage.get_requested_emulation_run(1)
         assert requested_emulation_run.status == EmulationStatus.REQUESTED
-        assert requested_emulation_run.emulation_id == 1
-        # Checking port?
+        assert requested_emulation_run.emulation_id == test_add_emulation_to_db
+        assert test_emulation_storage._is_port_in_use(requested_emulation_run.port)
 
     def test_set_error_run_status(self, test_emulation_storage, test_add_emulation_to_db):
-        emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(1, "admin")
+        emulation_run: db.EmulationRun = test_emulation_storage.create_emulation_run(
+            emulation_id=test_add_emulation_to_db, initiated_by=ADMIN
+        )
         assert emulation_run.status == EmulationStatus.CREATED
-        test_emulation_storage.set_error_emulation_run(1, "admin")
+        test_emulation_storage.set_error_emulation_run(emulation_run_id=emulation_run.id, traceback=Faker().sentence())
         assert emulation_run.status == EmulationStatus.ERROR
+        assert test_emulation_storage._is_port_in_use(emulation_run.port)
