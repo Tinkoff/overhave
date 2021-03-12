b173556d42097068ba42753ae1cf57df2ccbaaf2
l.ananenkov <>
2021-03-12 14:30:07 +0500

    assert

--- tests/unit/tag/test_tag.py
+++ tests/unit/tag/test_tag.py
@@ -12,7 +12,7 @@ class TestTagView:
     @pytest.mark.parametrize("user_role_mock", [db.Role.admin], indirect=True)
     def test_get_tag_created_by(self, test_tags_view, current_user_mock, test_tags_row):
         test_tags_view.on_model_change(form=mock.MagicMock(), model=test_tags_row, is_created=True)
-        assert test_tags_row.created_by
+        assert test_tags_row.created_by == current_user_mock.login
 
     @pytest.mark.parametrize("user_role_mock", [db.Role.admin], indirect=True)
     def test_get_tag_created_not_change(self, test_tags_view, current_user_mock, test_tags_row):
