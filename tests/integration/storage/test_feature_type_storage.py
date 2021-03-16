b541ec785b6cb1c8fa5fd4eaa41cd597b0ae2cf9
Artemy Rogov <57125077+artamaney@users.noreply.github.com>
2021-03-16 15:00:15 +0500

    fix

--- tests/integration/storage/test_feature_type_storage.py
+++ tests/integration/storage/test_feature_type_storage.py
@@ -1,14 +1,16 @@
 from faker import Faker
 
+from overhave.entities import FeatureTypeModel
 from overhave.storage.feature_type import FeatureTypeStorage
 
 
 class TestFeatureTypeStorage:
     """ Integration tests for :class:`FeatureTypeStorage`. """
 
-    def test_get_default_feature_type(self, test_feature_type_storage: FeatureTypeStorage, faker: Faker):
+    def test_get_default_feature_type(
+        self, test_feature_type_storage: FeatureTypeStorage, faker: Faker, feature_type: FeatureTypeModel
+    ):
+        name = feature_type.name
         feature_type_model = test_feature_type_storage.get_default_feature_type()
         assert feature_type_model is not None
-        name = faker.word()
-        feature_type_model.name = name
         assert feature_type_model.name == name
