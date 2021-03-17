97d2d7fea34372ffe110f337e96da9c400573a3b
Artemy Rogov <57125077+artamaney@users.noreply.github.com>
2021-03-17 17:57:55 +0500

    fix

--- tests/integration/storage/test_feature_type_storage.py
+++ tests/integration/storage/test_feature_type_storage.py
@@ -8,7 +8,9 @@ from overhave.storage.feature_type import FeatureTypeStorage
 class TestFeatureTypeStorage:
     """ Integration tests for :class:`FeatureTypeStorage`. """
 
-    def test_get_default_feature_type(self, test_feature_type_storage: FeatureTypeStorage, test_feature_type):
+    def test_get_default_feature_type(
+        self, test_feature_type_storage: FeatureTypeStorage, test_feature_type: FeatureTypeModel
+    ):
         feature_type_model = test_feature_type_storage.get_default_feature_type()
         assert feature_type_model is not None
         assert feature_type_model.name == test_feature_type.name
