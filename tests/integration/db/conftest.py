00eced044c5d6b338a9348472cef0903afe92d9b
a.rogov <arogov@nbubnt297.tcsbank.ru>
2021-04-20 12:20:30 +0700

    created tags factory except three functions

--- tests/integration/db/conftest.py
+++ tests/integration/db/conftest.py
@@ -6,7 +6,7 @@ from faker import Faker
 
 from overhave import db
 from overhave.db.tables import Tags
-from overhave.entities.converters import SystemUserModel, TagsTypeModel
+from overhave.entities.converters import SystemUserModel
 
 
 @pytest.fixture()
@@ -26,36 +26,11 @@ def test_system_user(database: None, faker: Faker, test_user_role: db.Role) -> S
 
 
 @pytest.fixture()
-def test_tags_with_spaces(faker: Faker, test_system_user: SystemUserModel):
-    def _test_tags_with_spaces() -> TagsTypeModel:
+def test_tags_factory(test_system_user: SystemUserModel):
+    def _test_tags(value: str) -> None:
         with db.create_session() as session:
-            tags = Tags(value="kek ", created_by=test_system_user.login)
+            tags = Tags(value=value, created_by=test_system_user.login)
             session.add(tags)
             session.flush()
-            return cast(TagsTypeModel, tags)
 
-    return _test_tags_with_spaces
-
-
-@pytest.fixture()
-def test_tags_with_special_symbols(faker: Faker, test_system_user: SystemUserModel):
-    def _test_tags_with_special_symbols() -> TagsTypeModel:
-        with db.create_session() as session:
-            tags = Tags(value="kek#!", created_by=test_system_user.login)
-            session.add(tags)
-            session.flush()
-            return cast(TagsTypeModel, tags)
-
-    return _test_tags_with_special_symbols
-
-
-@pytest.fixture()
-def test_correct_tags(faker: Faker, test_system_user: SystemUserModel):
-    def _test_correct_tags() -> TagsTypeModel:
-        with db.create_session() as session:
-            tags = Tags(value=faker.word(), created_by=test_system_user.login)
-            session.add(tags)
-            session.flush()
-            return cast(TagsTypeModel, tags)
-
-    return _test_correct_tags
+    return _test_tags
--- tests/integration/db/test_string_without_special_symbols.py
+++ tests/integration/db/test_string_without_special_symbols.py
@@ -1,27 +1,31 @@
 from typing import Callable
 
 import pytest
+from faker import Faker
 from sqlalchemy.exc import StatementError
 
 from overhave import db
-from overhave.entities.converters import TagsTypeModel
 
 
 class TestStringWithoutSpecialSymbols:
     """ Unit tests for :class:`StringWithoutSpecialSymbols`. """
 
     @pytest.mark.parametrize("test_user_role", [db.Role.admin], indirect=True)
-    def test_raises_value_error_if_space(self, test_tags_with_spaces: Callable[[], TagsTypeModel]) -> None:
+    def test_raises_value_error_if_space(self, test_tags_factory: Callable[[str], None], faker: Faker) -> None:
         with pytest.raises(StatementError):
-            test_tags_with_spaces()
+            test_tags_factory(f"{faker.word()} ")
+        with pytest.raises(StatementError):
+            test_tags_factory(f" {faker.word()}")
 
     @pytest.mark.parametrize("test_user_role", [db.Role.admin], indirect=True)
     def test_raises_value_error_if_special_symbols(
-        self, test_tags_with_special_symbols: Callable[[], TagsTypeModel]
+        self, test_tags_factory: Callable[[str], None], faker: Faker
     ) -> None:
         with pytest.raises(StatementError):
-            test_tags_with_special_symbols()
+            test_tags_factory(f"{faker.word()}#$!")
+        with pytest.raises(StatementError):
+            test_tags_factory("_")
 
     @pytest.mark.parametrize("test_user_role", [db.Role.admin], indirect=True)
-    def test_correct_value(self, test_correct_tags: Callable[[], TagsTypeModel]) -> None:
-        test_correct_tags()
+    def test_correct_value(self, test_tags_factory: Callable[[str], None], faker: Faker) -> None:
+        test_tags_factory(faker.word())
