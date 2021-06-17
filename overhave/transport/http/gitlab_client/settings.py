e0fe62abbc13dcde228ab77152499a78b635081e
a.rogov <a.rogov@macbook-a.rogov>
2021-06-17 16:45:12 +0500

    fix

--- overhave/transport/http/gitlab_client/settings.py
+++ overhave/transport/http/gitlab_client/settings.py
@@ -8,9 +8,11 @@ class OverhaveGitlabClientSettings(BaseHttpClientSettings):
 
     mr_path: str = "api/v4/projects/{repository_id}/merge_requests"
     auth_token: str
+    repository_id: str
 
     class Config:
         env_prefix = "OVERHAVE_GITLAB_"
 
-    def get_mr_url(self, repository_id: str) -> URL:
-        return self.url / self.mr_path.format(repository_id=repository_id)
+    @property
+    def get_mr_url(self) -> URL:
+        return self.url / self.mr_path.format(repository_id=self.repository_id)
