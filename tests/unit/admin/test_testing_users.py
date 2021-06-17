75b90d85f148ff36788fdc769b7278e865ca2159
a.rogov <a.rogov@macbook-a.rogov>
2021-06-17 11:24:17 +0500

    fixik

--- tests/unit/admin/test_testing_users.py
+++ tests/unit/admin/test_testing_users.py
@@ -30,14 +30,16 @@ class TestTestingUsers:
             test_testing_user_view.on_model_delete(model=test_testing_user_row)
 
     @pytest.mark.parametrize("user_role", [db.Role.user, db.Role.admin], indirect=True)
+    @pytest.mark.parametrize("test_is_created", [False, True])
     def test_incorrect_model_raises_error(
         self,
         test_testing_user_view: TestUserView,
         test_incorrect_testing_user_row: db.TestUser,
         current_testing_users_user_mock: mock.MagicMock,
         form_mock: mock.MagicMock,
+        test_is_created: bool,
     ) -> None:
         with pytest.raises(ValidationError):
             test_testing_user_view.on_model_change(
-                form=form_mock, model=test_incorrect_testing_user_row, is_created=True
+                form=form_mock, model=test_incorrect_testing_user_row, is_created=test_is_created
             )
