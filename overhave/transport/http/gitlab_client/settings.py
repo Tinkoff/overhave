e7248b9d932be92308a1eb751e8aadc202533f3d
a.rogov <a.rogov@macbook-a.rogov>
2021-07-26 17:07:21 +0500

    fix

--- overhave/transport/http/gitlab_client/settings.py
+++ overhave/transport/http/gitlab_client/settings.py
@@ -1,11 +1,5 @@
-from typing import Optional
-
-from pydantic import validator
-from yarl import URL
-
 from overhave.transport.http import BaseHttpClientSettings
 from overhave.transport.http.gitlab_client.utils import TokenType
-from overhave.utils import make_url
 
 
 class OverhaveGitlabClientSettings(BaseHttpClientSettings):
@@ -15,10 +9,6 @@ class OverhaveGitlabClientSettings(BaseHttpClientSettings):
     repository_id: str
     token_type: TokenType
     token: str
-
-    @validator("tokenizer_url", pre=True)
-    def make_url(cls, v: Optional[str]) -> Optional[URL]:
-        return make_url(v)
 
     class Config:
         env_prefix = "OVERHAVE_GITLAB_"
