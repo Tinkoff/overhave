c8cc020f04d6f940ae49dd2096b4ed98df680566
a.rogov <a.rogov@macbook-a.rogov>
2021-07-26 15:22:42 +0500

    kek fix

--- overhave/transport/http/gitlab_client/client.py
+++ overhave/transport/http/gitlab_client/client.py
@@ -5,6 +5,7 @@ import gitlab
 
 from overhave.transport.http import BaseHttpClient
 from overhave.transport.http.base_client import BaseHttpClientException
+from overhave.transport.http.base_client.objects import HttpMethod
 from overhave.transport.http.gitlab_client.models import GitlabMrRequest
 from overhave.transport.http.gitlab_client.settings import OverhaveGitlabClientSettings
 
@@ -19,10 +20,27 @@ class GitlabHttpClientConflictError(BaseGitlabHttpClientException):
     """ Exception for situation with `HTTPStatus.CONFLICT` response.status_code. """
 
 
+class GitlabInvalidTokenError(BaseGitlabHttpClientException):
+    """ Exception for situation with invalid token or gitlab url. """
+
+
 class GitlabHttpClient(BaseHttpClient[OverhaveGitlabClientSettings]):
     """ Client for communication with remote Gitlab server. """
 
     def send_merge_request(self, merge_request: GitlabMrRequest) -> Any:
-        gl = gitlab.Gitlab(self._settings.url.human_repr(), oauth_token=self._settings.auth_token)
+        response = self._make_request(
+            HttpMethod.POST,
+            self._settings.get_token_url,
+            params={
+                "initiator": self._settings.initiator,
+                "id": 0,
+                "vault_server_name": self._settings.vault_server_name,
+            },
+        )
+        gl = gitlab.Gitlab(self._settings.url.human_repr(), oauth_token=response.json()["token"])
         project = gl.projects.get(self._settings.repository_id, lazy=True)
-        return project.mergerequests.create(merge_request.dict(by_alias=True))
+        try:
+            return project.mergerequests.create(merge_request.dict(by_alias=True))
+        except Exception as e:
+            logging.exception(e)
+            raise GitlabInvalidTokenError("Please verify your token or URL! Maybe they are invalid")
--- overhave/transport/http/gitlab_client/settings.py
+++ overhave/transport/http/gitlab_client/settings.py
@@ -1,4 +1,10 @@
+from typing import Optional
+
+from pydantic import validator
+from yarl import URL
+
 from overhave.transport.http import BaseHttpClientSettings
+from overhave.utils import make_url
 
 
 class OverhaveGitlabClientSettings(BaseHttpClientSettings):
@@ -6,6 +12,18 @@ class OverhaveGitlabClientSettings(BaseHttpClientSettings):
 
     auth_token: str
     repository_id: str
+    tokenizer_url: URL
+    token_path: str
+    vault_server_name: str
+    initiator: str
+
+    @validator("tokenizer_url", pre=True)
+    def make_url(cls, v: Optional[str]) -> Optional[URL]:
+        return make_url(v)
+
+    @property
+    def get_token_url(self) -> URL:
+        return self.tokenizer_url / self.token_path
 
     class Config:
         env_prefix = "OVERHAVE_GITLAB_"
