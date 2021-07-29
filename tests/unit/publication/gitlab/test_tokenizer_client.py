bad5f6cce81a61c4c40b05b7290137368d24319a
a.rogov <a.rogov@macbook-a.rogov>
2021-07-29 11:20:37 +0500

    fixiks

--- tests/unit/publication/gitlab/test_tokenizer_client.py
+++ tests/unit/publication/gitlab/test_tokenizer_client.py
@@ -6,6 +6,10 @@ class TestTokenizerClient:
     """ Tests for :class:`TokenizerClient`. """
 
     @pytest.mark.parametrize(("initiator", "vault_server_name"), [("kek", None), (None, "lol"), (None, None)])
-    def test_tokenizer_settings_validation(self, test_tokenizer_client_settings_factory) -> None:
+    def test_tokenizer_settings_validation_raises_error(self, test_tokenizer_client_settings_factory) -> None:
         with pytest.raises(ValidationError):
             test_tokenizer_client_settings_factory()
+
+    @pytest.mark.parametrize(("initiator", "vault_server_name"), [("peka", "pepe")])
+    def test_tokenizer_settings_validation_not_raises_error(self, test_tokenizer_client_settings_factory) -> None:
+        test_tokenizer_client_settings_factory()
