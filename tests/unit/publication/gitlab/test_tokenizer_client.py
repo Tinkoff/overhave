219346936cf1b172fe4c7967b80d06a3c23307cf
a.rogov <a.rogov@macbook-a.rogov>
2021-07-29 13:24:15 +0500

    fixiks

--- tests/unit/publication/gitlab/test_tokenizer_client.py
+++ tests/unit/publication/gitlab/test_tokenizer_client.py
@@ -1,15 +1,23 @@
+from typing import Callable
+
 import pytest
 from pydantic import ValidationError
+
+from overhave.publication.gitlab.tokenizer.settings import TokenizerClientSettings
 
 
 class TestTokenizerClient:
     """ Tests for :class:`TokenizerClient`. """
 
     @pytest.mark.parametrize(("initiator", "vault_server_name"), [("kek", None), (None, "lol"), (None, None)])
-    def test_tokenizer_settings_validation_raises_error(self, test_tokenizer_client_settings_factory) -> None:
+    def test_tokenizer_settings_validation_raises_error(
+        self, test_tokenizer_client_settings_factory: Callable[[], TokenizerClientSettings]
+    ) -> None:
         with pytest.raises(ValidationError):
             test_tokenizer_client_settings_factory()
 
     @pytest.mark.parametrize(("initiator", "vault_server_name"), [("peka", "pepe")])
-    def test_tokenizer_settings_validation_not_raises_error(self, test_tokenizer_client_settings_factory) -> None:
+    def test_tokenizer_settings_validation_not_raises_error(
+        self, test_tokenizer_client_settings_factory: Callable[[], TokenizerClientSettings]
+    ) -> None:
         test_tokenizer_client_settings_factory()
--- tests/unit/transport/http/gitlab/test_gitlab_python_client.py
+++ tests/unit/transport/http/gitlab/test_gitlab_python_client.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+import gitlab
 import pytest
 
 from overhave.transport.http.gitlab_client.utils import InvalidTokenTypeError
@@ -7,6 +10,8 @@ class TestGitlabPythonClient:
     """ Unit tests for gitlab-python utils. """
 
     @pytest.mark.parametrize(("token_type", "token"), [("gotcha", "peka")])
-    def test_get_gitlab_python_client_raises_error(self, test_gitlab_python_client_factory) -> None:
+    def test_get_gitlab_python_client_raises_error(
+        self, test_gitlab_python_client_factory: Callable[[], gitlab.Gitlab]
+    ) -> None:
         with pytest.raises(InvalidTokenTypeError):
             test_gitlab_python_client_factory()
