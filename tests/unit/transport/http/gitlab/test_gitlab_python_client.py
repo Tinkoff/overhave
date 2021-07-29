fe60e4d8a8dd70d954f940dff3064249cd63f584
a.rogov <a.rogov@macbook-a.rogov>
2021-07-29 11:37:06 +0500

    fixiks

---
+++ tests/unit/transport/http/gitlab/test_gitlab_python_client.py
@@ -0,0 +1,12 @@
+import pytest
+
+from overhave.transport.http.gitlab_client.utils import InvalidTokenTypeError
+
+
+class TestGitlabPythonClient:
+    """ Unit tests for gitlab-python utils """
+
+    @pytest.mark.parametrize(("token_type", "token"), [("gotcha", "peka")])
+    def test_get_gitlab_python_client_raises_error(self, test_gitlab_python_client_factory):
+        with pytest.raises(InvalidTokenTypeError):
+            test_gitlab_python_client_factory()
