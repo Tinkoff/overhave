252c014db2e3bad0236fe08150a86d7815a7398b
a.rogov <a.rogov@macbook-a.rogov>
2021-07-29 13:25:14 +0500

    fixiks

--- overhave/publication/gitlab/tokenizer/settings.py
+++ overhave/publication/gitlab/tokenizer/settings.py
@@ -18,7 +18,7 @@ class TokenizerClientSettings(BaseHttpClientSettings):
         env_prefix = "GITLAB_TOKENIZER_"
 
     @validator("vault_server_name", "initiator")
-    def validate_vault_server_name(cls, v: Optional[str], values: Dict[str, Any]):
+    def validate_vault_server_name(cls, v: Optional[str], values: Dict[str, Any]) -> Optional[str]:
         if values["enabled"] and not isinstance(v, str):
             raise ValueError("Please verify GITLAB_TOKENIZER_VAULT_SERVER_NAME! Maybe you've forgotten about this env!")
         return v
