aeac14e57ccc128aa7ed15f5eb0953f4217c1274
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2021-08-10 10:20:44 +0500

    Added test for feature creation

--- tests/integration/storage/test_feature_storage.py
+++ tests/integration/storage/test_feature_storage.py
@@ -1,8 +1,26 @@
+from uuid import uuid1
+
 import pytest
 
 from overhave import db
 from overhave.entities import FeatureModel, FeatureTypeModel
 from overhave.storage import FeatureStorage
+
+
+def _check_base_feature_attrs(test_model: FeatureModel, validation_model: FeatureModel) -> None:
+    assert test_model.name == validation_model.name
+    assert test_model.author == validation_model.author
+    assert test_model.type_id == validation_model.type_id
+    assert test_model.file_path == validation_model.file_path
+    assert test_model.task == validation_model.task
+    assert test_model.last_edited_by == validation_model.last_edited_by
+    assert test_model.released == validation_model.released
+
+
+def _check_base_feature_type_attrs(test_model: FeatureTypeModel, validation_model: FeatureTypeModel) -> None:
+    assert test_model is not None
+    assert test_model.id == validation_model.id
+    assert test_model.name == validation_model.name
 
 
 @pytest.mark.usefixtures("database")
@@ -16,13 +34,18 @@ class TestFeatureStorage:
         feature_model = test_feature_storage.get_feature(test_feature.id)
         assert feature_model is not None
         assert feature_model.id == test_feature.id
-        assert feature_model.name == test_feature.name
-        assert feature_model.author == test_feature.author
-        assert feature_model.type_id == test_feature.type_id
-        assert feature_model.file_path == test_feature.file_path
-        assert feature_model.task == test_feature.task
-        assert feature_model.last_edited_by == test_feature.last_edited_by
-        assert feature_model.released == test_feature.released
-        assert feature_model.feature_type is not None
-        assert feature_model.feature_type.id == test_feature_type.id
-        assert feature_model.feature_type.name == test_feature_type.name
+        _check_base_feature_attrs(test_model=feature_model, validation_model=test_feature)
+        _check_base_feature_type_attrs(test_model=feature_model.feature_type, validation_model=test_feature_type)
+
+    def test_create_feature(
+        self, test_feature_storage: FeatureStorage, test_feature_type: FeatureTypeModel, test_feature: FeatureModel
+    ) -> None:
+        test_feature.name = uuid1().hex
+        test_feature.file_path = uuid1().hex
+        with db.create_session() as session:
+            feature_id = test_feature_storage.create_feature(session=session, model=test_feature)
+        feature_model = test_feature_storage.get_feature(feature_id)
+        assert feature_model is not None
+        assert feature_model.id == feature_id
+        _check_base_feature_attrs(test_model=feature_model, validation_model=test_feature)
+        _check_base_feature_type_attrs(test_model=feature_model.feature_type, validation_model=test_feature_type)
