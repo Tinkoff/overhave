aa9acfca5cd22382ec4f9ee958dded5bf67ffe4b
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2021-08-25 12:32:29 +0500

    add mock

--- overhave/admin/views/__init__.py
+++ overhave/admin/views/__init__.py
@@ -3,7 +3,8 @@ from .access import GroupView, UserView
 from .draft import DraftView
 from .emulation import EmulationView
 from .emulation_run import EmulationRunView
-from .feature import FactoryViewUtilsMixin, FeatureView
+from .feature import FeatureView
+from .feature import FactoryViewUtilsMixin
 from .formatters import datetime_formatter, result_report_formatter, task_formatter
 from .index import OverhaveIndexView
 from .scenario_test_run import TestRunView
