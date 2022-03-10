11ed24cfe004f5eb2bc15db006d93a0b4f6382cd
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2022-03-10 10:40:36 +0500

    Added tests for put spec

--- tests/integration/api/conftest.py
+++ tests/integration/api/conftest.py
@@ -2,8 +2,14 @@ import pytest
 from fastapi.testclient import TestClient
 
 from overhave import overhave_api
+from overhave.entities import TestUserSpecification
 
 
 @pytest.fixture()
 def test_api_client(database: None) -> TestClient:
     return TestClient(overhave_api())
+
+
+@pytest.fixture(scope="module")
+def test_new_specification() -> TestUserSpecification:
+    return TestUserSpecification({"new_test": "new_value"})
