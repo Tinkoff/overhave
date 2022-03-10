0161d7cb87f94c5e5dd60f540e92034223e8edd8
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2022-03-10 10:11:53 +0500

    added spec func for api

--- overhave/api/views.py
+++ overhave/api/views.py
@@ -5,8 +5,8 @@ from typing import Optional
 import fastapi
 
 from overhave.api.deps import get_test_user_storage
-from overhave.entities.converters import TestUserModel
-from overhave.storage import ITestUserStorage
+from overhave.entities.converters import TestUserModel, TestUserSpecification
+from overhave.storage import ITestUserStorage, TestUserDoesNotExistError
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ def _test_user_id_handler(user_id: int, test_user_storage: ITestUserStorage) -> 
     logger.info("Getting %s with user_id=%s...", TestUserModel.__name__, user_id)
     test_user = test_user_storage.get_test_user_by_id(user_id)
     if test_user is None:
-        raise fastapi.HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"User with id={user_id} does not exist")
+        raise fastapi.HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=f"User with id={user_id} does not exist")
     return test_user
 
 
@@ -24,7 +24,7 @@ def _test_user_name_handler(user_name: str, test_user_storage: ITestUserStorage)
     test_user = test_user_storage.get_test_user_by_name(user_name)
     if test_user is None:
         raise fastapi.HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail=f"User with name='{user_name}' does not exist"
+            status_code=HTTPStatus.BAD_REQUEST, detail=f"User with name='{user_name}' does not exist"
         )
     return test_user
 
@@ -41,3 +41,23 @@ def test_user_handler(
     raise fastapi.HTTPException(
         status_code=HTTPStatus.BAD_REQUEST, detail="'user_id' or 'user_name' query parameter should be set"
     )
+
+
+def test_user_get_spec_handler(
+    user_id: int, test_user_storage: ITestUserStorage = fastapi.Depends(get_test_user_storage)
+) -> TestUserSpecification:
+    test_user = _test_user_id_handler(user_id=user_id, test_user_storage=test_user_storage)
+    return test_user.specification
+
+
+def test_user_put_spec_handler(
+    user_id: int,
+    specification: TestUserSpecification,
+    test_user_storage: ITestUserStorage = fastapi.Depends(get_test_user_storage),
+) -> None:
+    logger.info("Updating %s for user_id=%s...", TestUserSpecification.__name__, user_id)
+    try:
+        test_user_storage.update_test_user_specification(user_id=user_id, specification=specification)
+    except TestUserDoesNotExistError:
+        raise fastapi.HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=f"User with id={user_id} does not exist")
+    logger.info("%s for user_id=%s was successfully updated", TestUserSpecification.__name__, user_id)
--- tests/integration/api/test_testusers.py
+++ tests/integration/api/test_testusers.py
@@ -17,7 +17,7 @@ class TestTestUserAPi:
 
     def test_get_user_by_id_empty(self, test_api_client: TestClient, faker: Faker, test_user_role: db.Role) -> None:
         response = test_api_client.get(f"/test_user/?user_id={faker.random_int()}")
-        assert response.status_code == 404
+        assert response.status_code == 400
         assert response.content is not None
 
     def test_get_user_by_id(self, test_api_client: TestClient, test_testuser: TestUserModel) -> None:
@@ -29,7 +29,7 @@ class TestTestUserAPi:
 
     def test_get_user_by_name_empty(self, test_api_client: TestClient, faker: Faker, test_user_role: db.Role) -> None:
         response = test_api_client.get(f"/test_user/?user_name={faker.random_int()}")
-        assert response.status_code == 404
+        assert response.status_code == 400
         assert response.content is not None
 
     def test_get_user_by_name(self, test_api_client: TestClient, test_testuser: TestUserModel) -> None:
@@ -38,3 +38,17 @@ class TestTestUserAPi:
         assert response.json()
         obj = TestUserModel.parse_obj(response.json())
         assert obj == test_testuser
+
+    def test_get_user_spec_no_query(self, test_api_client: TestClient, test_user_role: db.Role) -> None:
+        response = test_api_client.get("/test_user//specification")
+        assert response.status_code == 404
+
+    def test_get_user_spec_empty(self, test_api_client: TestClient, faker: Faker, test_user_role: db.Role) -> None:
+        response = test_api_client.get(f"/test_user/{faker.random_int()}/specification")
+        assert response.status_code == 400
+        assert response.content is not None
+
+    def test_get_user_spec(self, test_api_client: TestClient, test_testuser: TestUserModel) -> None:
+        response = test_api_client.get(f"/test_user/{test_testuser.id}/specification")
+        assert response.status_code == 200
+        assert response.json() == test_testuser.specification
