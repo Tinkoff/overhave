9b7e73f9a57298ecf10635d355e25793adb99f9d
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2022-05-02 22:54:35 +0500

    upd handler

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CODE = overhave
 VENV ?= .venv
 WORK_DIR ?= .
-MIN_COVERAGE ?= 84.8
+MIN_COVERAGE ?= 84.9
 BUILD_DIR ?= dist
 PYTHON_VERSION ?= 3.10
 
--- overhave/api/views/feature_views.py
+++ overhave/api/views/feature_views.py
@@ -4,31 +4,26 @@ from typing import List, Optional
 
 import fastapi
 
-from overhave.api.deps import get_draft_storage, get_feature_storage, get_feature_tag_storage
+from overhave.api.deps import get_feature_storage, get_feature_tag_storage
 from overhave.api.views.tags_views import tags_item_handler
-from overhave.storage import FeatureModel, IDraftStorage, IFeatureStorage, IFeatureTagStorage
+from overhave.storage import FeatureModel, IFeatureStorage, IFeatureTagStorage
 
 logger = logging.getLogger(__name__)
-
-
-def _get_features_by_tag_id_handler(tag_id: int, feature_storage: IFeatureStorage) -> List[FeatureModel]:
-    logger.info("Getting %s by tag_id=%s...", FeatureModel.__name__, tag_id)
-    return feature_storage.get_features_by_tag(tag_id=tag_id)
 
 
 def get_features_handler(
     tag_id: Optional[int] = None,
     tag_value: Optional[str] = None,
-    has_releases: bool = True,
     feature_storage: IFeatureStorage = fastapi.Depends(get_feature_storage),
     tag_storage: IFeatureTagStorage = fastapi.Depends(get_feature_tag_storage),
-    draft_storage: IDraftStorage = fastapi.Depends(get_draft_storage),
 ) -> List[FeatureModel]:
     if tag_id is not None:
-        return _get_features_by_tag_id_handler(tag_id=tag_id, feature_storage=feature_storage)
+        logger.info("Getting %s by tag_id=%s...", FeatureModel.__name__, tag_id)
+        return feature_storage.get_features_by_tag(tag_id=tag_id)
     if tag_value is not None:
+        logger.info("Getting %s by tag_value=%s...", FeatureModel.__name__, tag_value)
         tag_model = tags_item_handler(value=tag_value, feature_tag_storage=tag_storage)
-        return _get_features_by_tag_id_handler(tag_id=tag_model.id, feature_storage=feature_storage)
+        return feature_storage.get_features_by_tag(tag_id=tag_model.id)
     raise fastapi.HTTPException(
         status_code=HTTPStatus.BAD_REQUEST, detail="'tag_id' or 'tag_value' query parameter should be set"
     )
