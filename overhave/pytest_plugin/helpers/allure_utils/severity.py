27cd9fcf2b01a82e4b0eac650923f3478bbd5659
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2022-05-08 11:26:09 +0500

    severity parsing

--- overhave/pytest_plugin/helpers/allure_utils/severity.py
+++ overhave/pytest_plugin/helpers/allure_utils/severity.py
@@ -1,14 +1,22 @@
-from typing import TextIO
+import re
+from functools import cache
+from typing import TextIO, Pattern
 
 import allure
+
+
+@cache
+def _get_severity_pattern(keyword: str) -> Pattern[str]:
+    return re.compile(rf"({keyword})(?P<severity>\w+)\b")
 
 
 def _get_severity_level(file_wrapper: TextIO, keyword: str) -> allure.severity_level:
     for line in file_wrapper:
         if keyword not in line:
             continue
-        severity = line.split(keyword)[-1]
-        return allure.severity_level(severity.strip())
+        parsed_severity = _get_severity_pattern(keyword).search(line)
+        if parsed_severity is not None:
+            return allure.severity_level(parsed_severity.group("severity"))
     return allure.severity_level.NORMAL
 
 
