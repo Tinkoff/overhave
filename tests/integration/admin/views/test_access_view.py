2af6c4591f22b9586c82542b4e612e8977ec626e
m.shkut <m.shkut@tinkoff.ru>
2023-03-06 19:19:04 +0500

    access view tests

--- tests/integration/admin/views/test_access_view.py
+++ tests/integration/admin/views/test_access_view.py
@@ -10,12 +10,12 @@ class TestAccessView:
     """Tests for access view depending on user role."""
 
     @pytest.mark.parametrize("test_user_role", [db.Role.admin], indirect=True)
-    def test_get_access_view_admin(self, test_client: FlaskClient, test_authorized_user: SystemUserModel):
+    def test_get_access_view_admin(self, test_client: FlaskClient, test_authorized_user: SystemUserModel) -> None:
         response = test_client.get("/userrole/")
         assert "Access - Users - Overhave" in response.data.decode("utf-8"), "Can find Access button"
 
     @pytest.mark.parametrize("test_user_role", [db.Role.user], indirect=True)
-    def test_get_access_view_user(self, test_client: FlaskClient, test_authorized_user: SystemUserModel):
+    def test_get_access_view_user(self, test_client: FlaskClient, test_authorized_user: SystemUserModel) -> None:
         response = test_client.get("/userrole/")
         assert 'You should be redirected automatically to the target URL: <a href="/">/</a>' in response.data.decode(
             "utf-8"
