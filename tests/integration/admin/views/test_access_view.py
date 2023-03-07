e5a5b2b16fa20df25cfd8a925d6d9c2105a47e56
m.shkut <m.shkut@tinkoff.ru>
2023-03-07 11:57:24 +0500

    access view tests

--- tests/integration/admin/views/test_access_view.py
+++ tests/integration/admin/views/test_access_view.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from flask.testing import FlaskClient
 
@@ -12,11 +14,13 @@ class TestAccessView:
     @pytest.mark.parametrize("test_user_role", [db.Role.admin], indirect=True)
     def test_get_access_view_admin(self, test_client: FlaskClient, test_authorized_user: SystemUserModel) -> None:
         response = test_client.get("/userrole/")
-        assert "Access - Users - Overhave" in response.data.decode("utf-8"), "Can find Access button"
+        assert response.status_code == HTTPStatus.OK
+        assert "Access - Users - Overhave" in response.data.decode("utf-8"), "Can't find Access button"
 
     @pytest.mark.parametrize("test_user_role", [db.Role.user], indirect=True)
     def test_get_access_view_user(self, test_client: FlaskClient, test_authorized_user: SystemUserModel) -> None:
         response = test_client.get("/userrole/")
+        assert response.status_code == HTTPStatus.FOUND
         assert 'You should be redirected automatically to the target URL: <a href="/">/</a>' in response.data.decode(
             "utf-8"
-        ), "Redirects to the index page"
+        ), "Doesn't redirect to the index page"
