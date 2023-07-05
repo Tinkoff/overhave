ae6fb06636eb0f074ddccf99ce9cc9348d8e27c9
t.kopteva <t.kopteva@tinkoff.ru>
2023-07-05 15:37:09 +0500

    remove file settings from unit tests

--- tests/unit/publication/stash/conftest.py
+++ tests/unit/publication/stash/conftest.py
@@ -60,7 +60,6 @@ def test_stash_publisher_with_default_reviewers(
     mocker: MockFixture,
 ) -> StashVersionPublisher:
     return StashVersionPublisher(
-        file_settings=test_file_settings,
         project_settings=test_project_settings,
         feature_storage=mocker.create_autospec(IFeatureStorage),
         scenario_storage=mocker.create_autospec(IScenarioStorage),
